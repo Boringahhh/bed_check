6d32517980f744dd7ddf2c291e034fcc04b1553b
Boringahhh <54703177+Boringahhh@users.noreply.github.com>
2024-10-16 21:32:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,11 +30,8 @@ class CQ(feapder.AirSpider):
         login_url = "https://ids.gzist.edu.cn/lyuapServer/v1/tickets"
         uid = response.json["uid"]
         code_base64_str = response.json["content"].split(",")[-1]
-        if code_base64_str == '-1':
-            code_result = None
-        else:
-            code, code_result = self.code_ocr(code_base64_str)
-            log.info(f"验证码: {code};答案: {code_result}")
+        code, code_result = self.code_ocr(code_base64_str)
+        log.info(f"验证码: {code};答案: {code_result}")
         post_data = {
             "username": USERNAME,
             "password": self.encrypt_password(PASSWORD),
